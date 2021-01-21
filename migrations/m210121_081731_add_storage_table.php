d6b49d940ffce1703cf65ce8b6c3b985e2613d55
Daarimas <daarimas@yandex.ru>
2021-01-21 11:24:51 +0300

    migrate

---
+++ migrations/m210121_081731_add_storage_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m210121_081731_add_storage_table
+ */
+class m210121_081731_add_storage_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%storage}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'count' => $this->integer(10),
+        ]);
+        $this->alterColumn('{{%storage}}', 'id', $this->integer(11).'AUTO_INCREMENT');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m210121_081731_add_storage_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m210121_081731_add_storage_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
